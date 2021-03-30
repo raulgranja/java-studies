395809641ce9ba3c8aabdca8ae086fd739c30798
raulgranja <radoglio@gmail.com>
2021-03-30 15:56:55 -0300

    Update 30/03/2021

---
+++ HelloWorldJavaFX/src/helloworldjavafx/FXMLDocumentController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldjavafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Raul
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label lblMessage;
+    private Button btnClick;
+    
+            
+    @FXML
+    private void clickedButton(ActionEvent event) {
+        lblMessage.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
