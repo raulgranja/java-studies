c10bf8d5f2f692e17f70985b9e20486b66f37142
raulgranja <radoglio@gmail.com>
2021-04-03 21:38:07 -0300

    Update 03/04/2021

---
+++ PrimalTypes/src/primaltypes/PrimalTypes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primaltypes;
+
+/**
+ *
+ * @author Raul
+ */
+public class PrimalTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "Raul";
+        float grade = 8.5f;
+        System.out.println("The grade is " + grade);
+        System.out.printf("%s's grade is %.4f \n", name, grade);
+    }
+    
+}
